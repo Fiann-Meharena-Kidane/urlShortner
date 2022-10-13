026ad2fd4d5021d939872f778441a2409f0a9fd9
finan <finanmeharenakidane@gmail.com>
2022-10-13 16:52:50 +0400

    fixed copy issues, added redirect function using js, prevented url duplication,

--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -13,17 +13,25 @@ def index(request):
         link=request.POST['link']
         short_link='http://127.0.0.1:8000/go/'
         letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-        for i in range(6):
-            short_link+=choice(letters)
+        
+        all_link_objects=Url.objects.all()
+        all_links=[object.original_link for object in all_link_objects]
+        
+        if not link in all_links: # if its new link, 
+            for i in range(6):
+                short_link+=choice(letters)
             
-        new_link=Url(original_link=link,
-                     short_link=short_link)
+            new_link=Url(original_link=link,
+                        short_link=short_link)
+            new_link.save()
+            
+            return render(request, 'index.html', {'link':link, 'short_link':short_link})
         
-        new_link.save()
-        
-        return render(request, 'index.html', {'link':link, 'short_link':short_link})
-        # return redirect('login')
+        else: # if its old link, just return the saved link from db, 
+            
+            my_url=Url.objects.get(original_link=link)
+            return render(request, 'index.html', {'link':link, 'short_link':my_url.short_link} )
+             
     else:
         return render(request,'index.html')
 
@@ -34,7 +42,7 @@ def login(request):
 
 def go(request, url):
     
-    short_link='http://127.0.0.1:8000/go/'+url
+    short_link='http://127.0.0.1:8000/go/' + url
     original_link=Url.objects.get(short_link=short_link)
      
     return render (request,'form.html', context={
--- urlshortner/static/scripts/index.js
+++ urlshortner/static/scripts/index.js
@@ -3,11 +3,14 @@ const copied_El=document.querySelector('.copied')
 
 short_linkEl.addEventListener('click', (target)=>{
 
+         if(short_linkEl.value){  // if the field has text, 
+
          navigator.clipboard.writeText(short_linkEl.value);
          copied_El.innerText='Copied!!'
          copied_El.style.color='dark'
          setTimeout(() => {
             copied_El.innerText=''
-         }, 1000);
+         }, 1000); 
+      }
 
 })
