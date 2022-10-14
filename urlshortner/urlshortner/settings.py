f97627b055c2a50a740609d1a25c63fe2dec4251
finan <finanmeharenakidane@gmail.com>
2022-10-14 21:31:01 +0400

    added procfile, and some packages before depolying to Heroku,

--- urlshortner/urlshortner/settings.py
+++ urlshortner/urlshortner/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hp2for)(f%br%!@+1!bbgz=6p36#e12fdmtz71(3-dz6o(jw-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'finan-Linkmini']
 
 
 # Application definition
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
